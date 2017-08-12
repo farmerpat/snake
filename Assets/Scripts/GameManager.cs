a1ebd6eba1606716535eb20dbfad80eb54653b77
Patrick Connelly <patricksparbanieconnelly@gmail.com>
2017-08-11 20:37:39 -0400

    prevent food from being dropped in snake

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -42,26 +42,43 @@ public class GameManager : MonoBehaviour {
 		// a different way would be to have the grid basically keep
 		// a copy of the game board. e.g. it would show the places
 		// where the snake already is
+
 		int xMul = Random.Range(-4, 5);
 		int yMul = Random.Range (-2, 3);
+		Vector2 tryFirstCorner = new Vector2 ();
+		Vector2 tryOtherCorner = new Vector2 ();
 
-		Debug.Log ("xMul:");
-		Debug.Log (xMul);
-		Debug.Log ("yMul:");
-		Debug.Log (yMul);
-		Debug.Log ("bounds:");
-		SpriteRenderer sr = foodPellet.GetComponent<SpriteRenderer> ();
-		Debug.Log (sr.sprite.bounds);
-		Debug.Log ("rect:");
-		Debug.Log (sr.sprite.rect);
+		float halfLength = .55f / 2;
+		float halfWidth = halfLength;
+		tryFirstCorner.x = (xMul * .55f) + halfLength;
+		tryFirstCorner.y = (yMul * .55f) + halfWidth;
+
+		tryOtherCorner.x = (xMul * .55f) - halfLength;
+		tryOtherCorner.y = (yMul * .55f) - halfWidth;
+
+		Collider2D spaceOccupied = Physics2D.OverlapArea (tryFirstCorner, tryOtherCorner);
+
+		while (spaceOccupied != null) {
+			xMul = Random.Range(-4, 5);
+		    yMul = Random.Range (-2, 3);
+
+			tryFirstCorner.x = (xMul * .55f) + halfLength;
+			tryFirstCorner.y = (yMul * .55f) + halfWidth;
+
+			tryOtherCorner.x = (xMul * .55f) - halfLength;
+			tryOtherCorner.y = (yMul * .55f) - halfWidth;
+
+			spaceOccupied = Physics2D.OverlapArea (tryFirstCorner, tryOtherCorner);
+
+		}
+
+//		Debug.Log ("bounds:");
+//		SpriteRenderer sr = foodPellet.GetComponent<SpriteRenderer> ();
+//		Debug.Log (sr.sprite.bounds);
+//		Debug.Log ("rect:");
+//		Debug.Log (sr.sprite.rect);
 
 		return new Vector3 (xMul * .55f, yMul * .55f, 0.0f);
-		// x has 1.4 irl
-		// x needs either
-		// 1.73 (+ .33)
-		// or
-		// 1.173 (- .23)
-		// wtf
 
 		// make sure its in bounds and is not in the snake!
 	}
